159fde44dd336312b511bb5500918c9af9ab93e8
Sulaiman Barki <sulaimanbarki@gmail.com>
2023-09-08 07:55:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const apiRoutes = require('./routes/apiRoutes');
+const cors = require('cors');
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors(
-    {
-        origin: "*",
-    }
-));
 
 app.use('/api', apiRoutes);
 app.use('/', function (req, res) {
