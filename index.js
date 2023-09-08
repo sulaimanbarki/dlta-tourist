277769ef839804c2399f100911aafdf7a8e451e7
Sulaiman Barki <sulaimanbarki@gmail.com>
2023-09-08 14:31:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/apiRoutes');
+const cors = require('cors');
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 const app = express();
-
-// Import the allowCors middleware
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-        return
-    }
-    return await fn(req, res)
-}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Apply the allowCors middleware to all routes
-app.use(allowCors);
 
 app.use('/api', apiRoutes);
 app.use('/', function (req, res) {
