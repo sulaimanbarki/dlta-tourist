797ce875c9e135fb6f1deb6da589d48bf6d6b11c
Sulaiman Barki <99899527+sulaimanbarki@users.noreply.github.com>
2023-12-25 20:32:19 +0500

    search api updated

--- controllers/APIsController.js
+++ controllers/APIsController.js
@@ -41,6 +41,81 @@ class APIsController {
             res.status(500).json({ error: error });
         }
     }
+
+    async onLoadGoogleAPISearch(req, res) {
+        try {
+            const { lat, lng, type } = req.query;
+            console.log(lat, lng, type);
+
+            const apiKey = process.env.GOOGLE_API_KEY || '';
+            let apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=500&type=${type}&key=${apiKey}`;
+
+            if (!apiKey) {
+                return res.status(401).json({ error: 'Server Error' });
+            }
+
+            const response = await axios.get(apiUrl, {
+                headers: {
+                    accept: 'application/json',
+                },
+            });
+            const data = await response.data;
+            return res.status(200).json(data);
+        } catch (error) {
+            res.status(500).json({ error: error });
+        }
+    }
+
+    async googlePlaceSearch(req, res) {
+        try {
+            const { searchText, near, radius } = req.query;
+            const apiKey = process.env.GOOGLE_API_KEY || '';
+            const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${near}&key=${apiKey}`;
+
+            const geocodingResponse = await axios.get(geocodingUrl, {
+                headers: {
+                    accept: 'application/json',
+                },
+            });
+            const geocodingData = await geocodingResponse.data;
+
+            if (geocodingData.results.length > 0) {
+                const location = geocodingData.results[0].geometry.location;
+                const apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${searchText}&location=${location.lat},${location.lng}&radius=${radius}&key=${apiKey}`;
+
+                const response = await axios.get(apiUrl, {
+                    headers: {
+                        accept: 'application/json',
+                    },
+                });
+                const data = await response.data;
+                return res.status(200).json(data);
+            } else {
+                return res.status(404).json({ error: 'Not Found' });
+            }
+
+        } catch (error) {
+            res.status(500).json({ error: error });
+        }
+    }
+
+    async placeDetails(req, res) {
+        const { placeId } = req.query;
+        const apiKey = process.env.GOOGLE_API_KEY || '';
+        const apiUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${apiKey}`;
+
+        try {
+            const response = await axios.get(apiUrl, {
+                headers: {
+                    accept: 'application/json',
+                },
+            });
+            const data = await response.data;
+            return res.status(200).json(data);
+        } catch (error) {
+            res.status(500).json({ error: error });
+        }
+    }
 }
 
 module.exports = new APIsController();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const APIsController = require('../controllers/APIsController');
 
 router.get('/mainSearch', APIsController.mainSearch);
+router.get('/onLoadGoogleAPISearch', APIsController.onLoadGoogleAPISearch);
+router.get('/googlePlaceSearch', APIsController.googlePlaceSearch);
+router.get('/googlePlaceDetails', APIsController.placeDetails);
 
 module.exports = router;
